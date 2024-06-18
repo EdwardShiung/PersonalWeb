8fc91d35e60876edaf1b49501e92fa3f542d1a2f
EdwardShiung <edwardshiung@gmail.com>
2024-06-18 16:07:05 -0400

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//Below it's for local MongoDB
+// mongoose.connect('mongodb://localhost:27017/express-mongo', {})
+// Connect the MongoDB Cloud
+mongoose.connect(process.env.MONGOURL)
+        .then(() => {
+            console.log("MongoDB is connected!")
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
 
 const app = express();
 
